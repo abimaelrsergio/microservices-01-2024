34d421b47a4c4498761dc304fd25ff69ac4efd14
abimaelrsergio <abimaelr.sergio@gmail.com>
2024-01-29 12:05:30 -0300

    new features

--- book-service/src/main/java/br/com/abimael/proxy/CambioProxy.java
+++ book-service/src/main/java/br/com/abimael/proxy/CambioProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.abimael.response.Cambio;
 
-@FeignClient(name = "cambio-servive", url = "localhost:8000")
+@FeignClient(name = "cambio-service")
 public interface CambioProxy {
 
 	@GetMapping("/cambio-service/{amount}/{from}/{to}")
